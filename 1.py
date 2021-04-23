9aed8c9470f938c9cda47a83d97684829540574f
s1nnayw0ng <82903861+s1nnayw0ng@users.noreply.github.com>
2021-04-23 18:23:48 +0100

    Update 1.py
    update on indentations
    (still have an error on 'hits')

--- 1.py
+++ 1.py
@@ -1,24 +1,22 @@
 import requests
 def recipe_search(ingredient):
 # Register to get an APP ID and key https://developer.edamam.com/
-app_id = '7ce35082'
-app_key = 'b9556c7fe5a71c999ae5db61af5beb8f'
-result = requests.get(
-'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id,
+    app_id = '7ce35082'
+    app_key = 'b9556c7fe5a71c999ae5db61af5beb8f'
+    result = requests.get(
+    'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id,app_key)
+    )
+    data = result.json()
+    return data['hits']
 
-app_key)
-)
-data = result.json()
-return data['hits']
 def run():
-ingredient = input('Enter an ingredient: ')
-results = recipe_search(ingredient)
-for result in results:
-recipe = result['recipe']
-print(recipe['label'])
-print(recipe['uri'])
-print()
+    ingredient = input('Enter an ingredient: ')
+    results = recipe_search(ingredient)
+    for result in results:
+        recipe = result['recipe']
+    print(recipe['label'])
+    print(recipe['uri'])
+    print()
 
-print('hello is this working')
-print()
 run()
+
