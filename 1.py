f8a0dce5b8a44f8d213da14cea861d8e00b2468a
Raina Greifer <rainagreifer@gmail.com>
2021-04-25 19:12:26 +0100

    edits to file including new import

--- 1.py
+++ 1.py
@@ -1,10 +1,11 @@
 import py_edamam
 import requests
 
-def recipe_search(ingredient):
-# Register to get an APP ID and key https://developer.edamam.com/
-    app_id = '7ce35082'
-    app_key = 'b9556c7fe5a71c999ae5db61af5beb8f'
+def _init_(ingredient, app_id, app_key):
+
+    def recipe_search(ingredient):
+        ingredient.app_id = '7ce35082'
+        ingredient.app_key = 'b9556c7fe5a71c999ae5db61af5beb8f'
 result = requests.get(
 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id,
 
