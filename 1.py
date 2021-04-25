a26c063fb09d84032ff5e272c02126479ba925ea
queerspaces <82678382+queerspaces@users.noreply.github.com>
2021-04-25 19:00:04 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,21 +1,24 @@
+import py_edamam
 import requests
+
 def recipe_search(ingredient):
+# Register to get an APP ID and key https://developer.edamam.com/
     app_id = '7ce35082'
     app_key = 'b9556c7fe5a71c999ae5db61af5beb8f'
-    result = requests.get(
-    'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id,app_key)
-    )
-    data = result.json()
-    return data['hits']
+result = requests.get(
+'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id,
 
+app_key)
+)
+data = result.json()
+return data['hits']
 def run():
     ingredient = input('Enter an ingredient: ')
     results = recipe_search(ingredient)
-    for result in results:
-        recipe = result['recipe']
-    print(recipe['label'])
-    print(recipe['uri'])
-    print()
-
+for result in results:
+    recipe = result['recipe']
+print(recipe['label'])
+print(recipe['uri'])
+print()
 run()
 
