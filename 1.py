0ef9326cb6026570e10a66636bcb1f24eef107a9
s1nnayw0ng <82903861+s1nnayw0ng@users.noreply.github.com>
2021-04-27 20:45:46 +0100

    Update 1.py
    Comments for existing code

--- 1.py
+++ 1.py
@@ -1,27 +1,52 @@
-#import library of recipes
+"""
+Project: CFG Python Project - Recipe search
+Description: Recipe search using Edamame API
+Created by: Raina Greifer and Yannis Wong
+"""
+
+# Importing libraries
+## Edamame API
 import py_edamam
-#importing ability to communicate with site
+## Requests - ability to interact with HTTP URL
 import requests
 
-#setting up search fuction with id and key so we can get data and will produce url
+# Main function to search for recipes from Edmame API
 def recipe_search(ingredient):
+
+    ## Setting up the URL at the backend with app id and key - Able to put 'ingredients' (input from users back to this for searches
     result = requests.get(
         'https://api.edamam.com/search?q={}&app_id=c457e479&app_key=30acb7fe7dbb730ae70f3b81f1ce1d9f'.format(ingredient)
     )
-    #makes the javascript information and data readable
+
+    ## Transform results to JavaScript Object Notation format - more readable and easier to process
     data = result.json()
-    #hits means matching results hits is part of api
+
+    ## Hits extracted from the Edamam API - extracting matching objects
     return data['hits']
 
-#what the user experiences
+# The key function that to be run
 def run():
+    
+    ## Asking user for input for ingredients that would like to cook with
     ingredient = input('Enter an ingredient: ')
+    
+    ## Substitute the user input back to the recipe_serach function
     results = recipe_search(ingredient)
 
-
+    ## Getting recipes that match the user-search criteria
     for result in results:
+        
+        ### Extracting the selected information from the data of JSON -
+        
+        #### Matching recipe in hit
         recipe = result['recipe']
+        
+        #### Recipe title in recipe
         print(recipe['label'])
+        
+        #### Link to recipe in Edamam
         print(recipe['shareAs'])
-        print()
+        
+
+# Executing the function        
 run()
